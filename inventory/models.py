c1c3b7b551dad56e9786b39ca7497dc88913e36f
npasqua <npasqua@cisco.com>
2013-07-01 10:37:36 -0400

    Fixed NameError on models.py, still good spacing and some comments

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+# Model to track individual Projects and some information about them
+class Project(models.Model):
+    name = models.CharField( max_length = 50, unique = True, )
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField( max_length = 50, )
+    information = models.CharField( max_length = 300, )
+
+    def __unicode__(self):
+        return self.name
+
 # Model to track individual hardware and it's information, including what Project 
 #  the hardware is a part of
 class Hardware(models.Model):
@@ -14,25 +25,12 @@ class Hardware(models.Model):
     financial_owner = models.IntegerField()
     status = models.CharField( max_length = 100, )
     serial_number = models.CharField( max_length = 50, unique = True)
-    parent_serial_number = models.ForeignKey('self', to_field='serial_number', 
-        blank = True, null = True, )
+    parent_serial_number = models.ForeignKey('self', to_field='serial_number', blank = True, null = True, )
     project_name = models.CharField( max_length = 100, )
     comment = models.CharField( max_length = 140, blank = True, )
 
     def __unicode__(self):
-        return 'eITMS: %s - Model: %s - S/N: %s - Project: %s' % 
-                (self.eITMS_code, self.model_code, self.serial_number,self.project.name)
-
-# Model to track individual Projects and some information about them
-class Project(models.Model):
-    name = models.CharField( max_length = 50, unique = True, )
-    start_date = models.DateField()
-    end_date = models.DateField()
-    status = models.CharField( max_length = 50, )
-    information = models.CharField( max_length = 300, )
-
-    def __unicode__(self):
-        return self.name
+        return 'eITMS: %s - Model: %s - S/N: %s - Project: %s' % (self.eITMS_code, self.model_code, self.serial_number,self.project.name)
 
 # Model to track users on all of the Projects
 class User(models.Model):
@@ -42,8 +40,7 @@ class User(models.Model):
     comment = models.CharField( max_length = 140, )
 
     def __unicode__(self):
-        return 'Name: %s - Email: %s - Company: %s' % 
-                (self.name, self.email, self.company)
+        return 'Name: %s - Email: %s - Company: %s' % (self.name, self.email, self.company)
 
 # Relational table to track which users are a part of which projects
 class Proj_Has_Users(models.Model):
@@ -52,16 +49,14 @@ class Proj_Has_Users(models.Model):
     role = models.CharField( max_length = 100, )
 
     def __unicode__(self):
-        return 'Project: %s - User: %s / %s' % 
-                (self.project.name, self.user.name, self.user.email)
+        return 'Project: %s - User: %s / %s' % (self.project.name, self.user.name, self.user.email)
 
 # Model to track the resource allocation of different hardware
 class Resources(models.Model):
     hardware = models.ForeignKey('Hardware')
-    cpu = CharField( max_length = 25, blank = True, )
-    storage = CharField( max_length = 25, blank = True,  )
-    memory = CharField( max_length = 25, blank = True, )
+    cpu = models.CharField( max_length = 25, blank = True, )
+    storage = models.CharField( max_length = 25, blank = True,  )
+    memory = models.CharField( max_length = 25, blank = True, )
 
     def __unicode__(self):
-        return 'eITMS: %s - Model: %s - CPU: %s - Storage: %s - Memory: %s' % 
-                (self.hardware.eITMS_code, self.hardware.model_code, self.cpu, self.storage, self.memory)
+        return 'eITMS: %s - Model: %s - CPU: %s - Storage: %s - Memory: %s' % (self.hardware.eITMS_code, self.hardware.model_code, self.cpu, self.storage, self.memory)
